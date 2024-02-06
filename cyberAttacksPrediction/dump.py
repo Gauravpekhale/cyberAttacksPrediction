fee504f40cd7eed3e0766e9514aa7ce3d4bf90b8
Gaurav Pekhale <89195306+Gauravpekhale@users.noreply.github.com>
2024-02-06 19:06:48 +0530

    Added Files

---
+++ cyberAttacksPrediction/dump.py
@@ -0,0 +1,23 @@
+#importing libraries
+
+import numpy as np
+import pandas as pd
+from joblib import dump, load
+import sklearn
+import sklearn.preprocessing
+from sklearn import metrics
+from scipy.stats import zscore
+from sklearn.metrics import accuracy_score, f1_score, precision_score,recall_score
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sys
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Embedding, Flatten
+from keras.layers import LSTM, SimpleRNN, GRU, Bidirectional, BatchNormalization,Convolution1D,MaxPooling1D, Reshape, GlobalAveragePooling1D
+from keras.utils import to_categorical
+from tensorflow.keras.utils import get_file, plot_model
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping
